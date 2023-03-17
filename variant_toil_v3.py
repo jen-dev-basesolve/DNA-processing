329841322cdcceb25a59966fdb34f6d9ed360d75
jen-dev-basesolve <125538981+jen-dev-basesolve@users.noreply.github.com>
2023-03-17 16:14:09 +0530

    added childJobFn and wrapperJobFn. Promises also added.

--- variant_toil_v3.py
+++ variant_toil_v3.py
@@ -6,11 +6,7 @@ import requests
 import shlex
 import subprocess
 
-
-def helloWorld(message,memory="2G", cores=2, disk="3G"):
-    return message
-
-def ref_genome(memory="2G", cores=2, disk="3G"):
+def ref_genome(job,memory="2G", cores=2, disk="3G"):
     parent_dir = '/home/bioinfo/singularity/variant_analysis'
     child_dir = 'dc_workshop_1/data/ref_genome'
     path = os.path.join(parent_dir,child_dir)
@@ -28,11 +24,13 @@ def ref_genome(memory="2G", cores=2, disk="3G"):
     # extracting fasta file
     comms = ("gunzip %s/%s"%(path,local_filename))
     os.popen(comms).read()
-
+    job.addChildJobFn(index_ref)
+    job.addFollowOnJobFn(fastq_files)
+    # return path
 
 
 # Downloading trimmed FastQ files for faster operations
-def fastq_files(memory="4G", cores=4, disk="5G"):
+def fastq_files(job,memory="4G", cores=4, disk="5G"):
     parent_dir = '/home/bioinfo/singularity/variant_analysis'       # tested, runs
     child_dir = 'dc_workshop_1/data/trimmed_fastq_small'            
     path = os.path.join(parent_dir,child_dir)
@@ -50,28 +48,27 @@ def fastq_files(memory="4G", cores=4, disk="5G"):
     comms = "cd %s/dc_workshop_1 ; mkdir -p results/sam results/bam results/bcf results/vcf"%parent_dir     # to be tested in this function
     os.popen(comms).read()     
     
-    # Not using job.log as it would involve using job function
-
+    job.addChildJobFn(align_fastq)
 
 # Indexing reference fasta file
-def index_ref(memory="2G", cores=2, disk="3G"):
+def index_ref(job,memory="2G", cores=2, disk="3G"):
     path = '/home/bioinfo/singularity/variant_analysis'
     comms = "singularity exec %s/bwa_latest.sif bwa index %s/dc_workshop_1/data/ref_genome/ecoli_rel606.fasta"%(path,path)
     process = os.popen(comms)
     process.read()
-
+    job.log("".join(["Following is the path >>>>",str(path)]))
 
 # Aligning sample sequences to reference genome
-def align_fastq(memory="2G", cores=2, disk="3G"):
+def align_fastq(job,memory="2G", cores=2, disk="3G"):
     path = '/home/bioinfo/singularity/variant_analysis'
     comms = "cd %s ; singularity exec bwa_latest.sif bwa mem dc_workshop_1/data/ref_genome/ecoli_rel606.fasta dc_workshop_1/data/trimmed_fastq_small/sub/SRR2584866_1.trim.sub.fastq dc_workshop_1/data/trimmed_fastq_small/sub/SRR2584866_2.trim.sub.fastq > dc_workshop_1/results/sam/SRR2584866.aligned.sam"%path
     process = os.popen(comms)
     process.read()
-
+    job.addChildJobFn(convrt_sort)
 
 # Converting SAM file to BAM file using view option in samtools
 # Sorting bam files
-def convrt_sort(memory="2G", cores=2, disk="3G"):
+def convrt_sort(job,memory="2G", cores=2, disk="3G"):
     path = '/home/bioinfo/singularity/variant_analysis'
     # convert sam file to bam
     comms = f"singularity exec {path}/samtools_latest.sif samtools view -S -b {path}/dc_workshop_1/results/sam/SRR2584866.aligned.sam > {path}/dc_workshop_1/results/bam/SRR2584866.aligned.bam"
@@ -82,9 +79,10 @@ def convrt_sort(memory="2G", cores=2, disk="3G"):
     print(comms)
     os.popen(comms).read()
     # runs, tested.
+    job.addChildJobFn(variant_call)
 
 # Variant calling
-def variant_call(memory="2G", cores=2, disk="3G"):
+def variant_call(job,memory="2G", cores=2, disk="3G"):
     path = '/home/bioinfo/singularity/variant_analysis'
     comms = f"singularity exec {path}/bcftools_latest.sif bcftools mpileup -O b -o {path}/dc_workshop_1/results/bcf/SRR2584866_raw.bcf -f {path}/dc_workshop_1/data/ref_genome/ecoli_rel606.fasta {path}/dc_workshop_1/results/bam/SRR2584866.aligned.sorted.bam"
     print(comms)
@@ -97,12 +95,16 @@ def variant_call(memory="2G", cores=2, disk="3G"):
     # Filter and report the SNV variants in variant calling format (VCF)
     comms = f"singularity exec {path}/bcftools_latest.sif vcfutils.pl varFilter {path}/dc_workshop_1/results/vcf/SRR2584866_variants.vcf  > {path}/dc_workshop_1/results/vcf/SRR2584866_final_variants.vcf"
     os.popen(comms).read()
+    job.addChildJobFn(index_fastq)
+
 
 # Indexing for visualization
-def index_fastq(memory="2G", cores=2, disk="3G"):
+def index_fastq(job,memory="2G", cores=2, disk="3G"):
     path = '/home/bioinfo/singularity/variant_analysis'
     comms = f"singularity exec {path}/samtools_latest.sif samtools index {path}/dc_workshop_1/results/bam/SRR2584866.aligned.sorted.bam"
     os.popen(comms).read()
+    
+
 
 # Visualising SNV using t_view
 # def t_view():
@@ -115,25 +117,9 @@ if __name__=='__main__':
     jobstore: str = tempfile.mkdtemp("test")
     os.rmdir(jobstore)
     options = Job.Runner.getDefaultOptions(jobstore)
-    options.logLevel='DEBUG'
+    options.logLevel='INFO'
 
-    j1 = Job.wrapFn(helloWorld,message='Hi There >>>>>>>>>>')
-    j2 = Job.wrapFn(ref_genome)
-    j3 = Job.wrapFn(fastq_files)
-    j4 = Job.wrapFn(index_ref)
-    j5 = Job.wrapFn(align_fastq)
-    j6 = Job.wrapFn(convrt_sort)
-    j7 = Job.wrapFn(variant_call)
-    j8 = Job.wrapFn(index_fastq)
-
-    j2.addChild(j3)
-    j3.addChild(j4)
-    j4.addChild(j5)
-    j5.addChild(j6)
-    j6.addChild(j7)
-    j7.addChild(j8)
-
+    j1 = Job.wrapJobFn(ref_genome)
 
     with Toil(options) as toil:
         print(toil.start(j1))
-        print(toil.start(j2))
